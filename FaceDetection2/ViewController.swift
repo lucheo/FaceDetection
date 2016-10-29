01700eddd9797900811410519f962dd24f8b11f8
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2016-10-29 16:11:41 -0200

    Initial Commit

---
+++ FaceDetection2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceDetection2
+//
+//  Created by Lucheo Antonio Tombini Filho on 29/10/16.
+//  Copyright © 2016 Lucheo Antonio Tombini Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
